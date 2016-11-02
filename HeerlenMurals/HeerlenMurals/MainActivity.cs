de81801fa96fdec6de76867fe1e418aeb09fc2c6
PandaFox12 <clan-rcon@hotmail.com>
2016-11-02 13:08:13 +0100

    mapje werkt

--- HeerlenMurals/HeerlenMurals/MainActivity.cs
+++ HeerlenMurals/HeerlenMurals/MainActivity.cs
@@ -1,18 +1,33 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Maps;
 
 namespace HeerlenMurals
 {
     [Activity(Label = "HeerlenMurals", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, IOnMapReadyCallback
     {
+        private GoogleMap GMap;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+            SetUpMap();
+        }
+        private void SetUpMap()
+        {
+            if (GMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.googlemap).GetMapAsync(this);
+            }
+        }
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            this.GMap = googleMap;
         }
     }
 }
